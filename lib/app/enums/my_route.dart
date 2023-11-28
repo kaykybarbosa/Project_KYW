89cf595cfff26362dce5c643daae194b29a1704e
kaykybarbosa <kayky@abrem.net>
2023-11-28 18:36:45 -0300

    Remove some files

--- lib/app/enums/my_route.dart
+++
@@ -1,8 +0,0 @@
-class MyRoute {
-  static String home = '/';
-  static String form = '/form';
-  static String forgotPassword = '/forgot-password';
-  static String changePassword = '/change-password';
-  static String projects = '/project';
-  static String notifications = '/notifications';
-}
