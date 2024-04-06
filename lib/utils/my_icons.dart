418bf8bf0173b9f64a84b570c8ea617dd1fcfa28
Kayky Barbosa <kaykyb629@gmail.com>
2024-04-06 16:40:21 -0300

    refactor: removendo alguns widgets

--- lib/utils/my_icons.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-abstract class MyIcons {
-  static const IconData bell = FontAwesomeIcons.bell;
-  static const IconData more = FontAwesomeIcons.ellipsisVertical;
-  static const IconData filter = FontAwesomeIcons.filter;
-}
