4c3719f606b2bdf3d7d10b6d742affdeee146fc3
Kayky Barbosa <kaykyb629@gmail.com>
2024-04-06 15:25:47 -0300

    feat: adicionando os icons

---
+++ lib/utils/my_icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+abstract class MyIcons {
+  static const IconData bell = FontAwesomeIcons.bell;
+  static const IconData more = FontAwesomeIcons.ellipsisVertical;
+  static const IconData filter = FontAwesomeIcons.filter;
+}
