c083be18fb120e8d11b1ee03aa0a068f9e3828ff
Kayky Barbosa <kaykyb629@gmail.com>
2024-04-28 16:10:15 -0300

    refactor: melhorando as options da tela

--- lib/ui/screens/configuration_screens/sub_pages/configuration_account_screen.dart
+++ lib/ui/screens/configuration_screens/sub_pages/configuration_account_screen.dart
@@ -1,70 +1,53 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
-import 'package:kyw_management/app/routers/my_routes.dart';
+import 'package:kyw_management/app/routers/app_pages/app_pages_exports.dart';
 import 'package:kyw_management/utils/colors.dart';
+import 'package:kyw_management/utils/icons.dart';
 
 class ConfigurationAccountScreen extends StatelessWidget {
   const ConfigurationAccountScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<String> optionsTitle = [
-      'Configuração de duas etapas',
-      'Mudar Número ou E-mail',
-      'Apagar conta',
-    ];
-    final List<IconData> optionsIcon = [
-      FontAwesomeIcons.shieldHalved,
-      FontAwesomeIcons.phoneFlip,
-      FontAwesomeIcons.solidTrashCan
-    ];
-
-    void goTwoStepVerification() => Get.toNamed(AppRoutes.twoStepVerification);
-
-    void goChangeNumberOrEmail() => Get.toNamed(AppRoutes.changeNumberOrEmail);
-
-    void deleteAccount() => Get.toNamed(AppRoutes.configurationDeleteAccount);
-
-    List<Function> optionsFunction = [
-      /// If [index] == 0 => show two_step_screen
-      /// If [index] == 1 => show change_number_screen
-      /// If [index] == 0 => show delete_account_screen
-
-      goTwoStepVerification,
-      goChangeNumberOrEmail,
-      deleteAccount
+    List<Map<String, dynamic>> options = [
+      {
+        'label': 'Configuração de duas etapas',
+        'icon': TIcons.security,
+        'color': TColors.secondary,
+        'onTap': () => Get.toNamed(AppRoutes.twoStepVerification),
+      },
+      {
+        'label': 'Apagar conta',
+        'icon': TIcons.trash,
+        'color': TColors.warn,
+        'onTap': () => Get.toNamed(AppRoutes.configurationDeleteAccount),
+      },
     ];
 
     return Scaffold(
       appBar: AppBar(title: const Text('Conta')),
       body: ListView.separated(
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Icon(
-              optionsIcon[index],
-              color: index != 2 ? TColors.secondary : CupertinoColors.destructiveRed,
+        itemBuilder: (context, index) => ListTile(
+          leading: Icon(
+            options[index]['icon'],
+            color: options[index]['color'],
+          ),
+          title: Text(
+            options[index]['label'],
+            style: const TextStyle(
+              fontSize: TConstants.fontSizeLg,
+              fontWeight: FontWeight.w500,
+              color: TColors.secondary,
             ),
-            title: Text(
-              optionsTitle[index],
-              style: const TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.w500,
-                color: TColors.secondary,
-              ),
-            ),
-            onTap: () {
-              optionsFunction[index]();
-            },
-          );
-        },
-        separatorBuilder: (__, _) => const Padding(
+          ),
+          onTap: options[index]['onTap'],
+        ),
+        separatorBuilder: (_, __) => const Padding(
           padding: EdgeInsets.symmetric(horizontal: 13.0),
-          child: Divider(height: 0.0),
+          child: Divider(),
         ),
         physics: const NeverScrollableScrollPhysics(),
-        itemCount: optionsTitle.length,
+        itemCount: options.length,
         padding: EdgeInsets.zero,
       ),
     );
