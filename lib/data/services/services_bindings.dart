6bf0f08e2151fd0e45e2d6bf3e4f8bb451443bb6
Kayky Barbosa <kaykyb629@gmail.com>
2024-05-05 19:33:55 -0300

    refactor: registrando instância do refreshTokenService

--- lib/data/services/services_bindings.dart
+++ lib/data/services/services_bindings.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
 import 'package:kyw_management/app/controllers/app_controller.dart';
+import 'package:kyw_management/data/repositories/auth/auth_repository.dart';
 import 'package:kyw_management/data/services/configure_login_service.dart';
+import 'package:kyw_management/data/services/refresh_token_service.dart';
 
 class ServicesBindings implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut<IAuthSettingsService>(() => AuthSettingsService(AppController.instance));
+    Get.lazyPut<IRefreshTokenService>(() => RefreshTokenService(authRepository: IAuthRepository.instance));
   }
 }
