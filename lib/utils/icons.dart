7c3a28b1518e54bdcf23b8b372945661510d37eb
Kayky Barbosa <kaykyb629@gmail.com>
2024-05-05 11:50:57 -0300

    feat: adicionando ícone

--- lib/utils/icons.dart
+++ lib/utils/icons.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 abstract class TIcons {
+  static const IconData logo = FontAwesomeIcons.thinkPeaks;
   static const IconData add = FontAwesomeIcons.plus;
   static const IconData xMarked = FontAwesomeIcons.x;
   static const IconData bell = FontAwesomeIcons.bell;
