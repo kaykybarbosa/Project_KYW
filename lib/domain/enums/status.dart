fca6189f3bb24c90cd9ade09cd9f48874c3c501c
Kayky Barbosa <kaykyb629@gmail.com>
2024-05-12 20:14:10 -0300

    feat: adicionando extension para recuperar o label do  enum

--- lib/domain/enums/status.dart
+++ lib/domain/enums/status.dart
@@ -1,1 +1,9 @@
 enum TaskStatus { complete, incomplete, pending }
+
+extension TaskStatusX on TaskStatus {
+  String get label => switch (this) {
+        TaskStatus.incomplete => 'Incompleto',
+        TaskStatus.pending => 'Pendente',
+        _ => 'Completo',
+      };
+}
