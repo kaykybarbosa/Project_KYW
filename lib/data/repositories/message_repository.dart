da0caecb4d5fe6d38812198c7fb4d6087765de05
Kayky Barbosa <kaykyb629@gmail.com>
2024-05-26 10:13:20 -0300

    feat: adicionando repository das mensagens

---
+++ lib/data/repositories/message_repository.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:kyw_management/data/services/http_service/http_service.dart';
+import 'package:kyw_management/domain/exception/api_exception.dart';
+import 'package:kyw_management/domain/models/message.dart';
+import 'package:result_dart/result_dart.dart';
+
+abstract class IMessageRepository {
+  static IMessageRepository get instance => Get.find();
+
+  AsyncResult<List<Message>, ApiException> getMessages({bool isDesc = true});
+}
+
+class MessageRepository implements IMessageRepository {
+  MessageRepository(IHttpService httpService) : _http = httpService;
+
+  final IHttpService _http;
+
+  @override
+  AsyncResult<List<Message>, ApiException> getMessages({bool isDesc = true}) async {
+    try {
+      final order = isDesc ? 'desc' : 'asc';
+
+      var result = await _http.get('${_http.baseUrl}/messages/sent/$order');
+
+      final messages = result.data['content'];
+
+      return messages.map((m) => Message.fromJson(m)).toList().toSuccess();
+    } on DioException catch (e) {
+      return ApiException(message: e.message).toFailure();
+    } catch (e) {
+      return ApiException(message: e.toString()).toFailure();
+    }
+  }
+}
--- lib/data/repositories/repositories_bindings.dart
+++ lib/data/repositories/repositories_bindings.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
+import 'package:kyw_management/data/repositories/message_repository.dart';
 import 'package:kyw_management/data/repositories/project_repository.dart';
 import 'package:kyw_management/data/services/http_service/http_service.dart';
 
 class RepositoriesBindings implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<IMessageRepository>(() => MessageRepository(IHttpService.instanceAuth));
     Get.lazyPut<IProjectRepository>(() => ProjectRepository(IHttpService.instanceAuth));
   }
 }
