6e31d930ee29934e6807d032e72d6a6d31e50572
Kayky Barbosa <kaykyb629@gmail.com>
2024-05-26 10:12:27 -0300

    style: aumentando fontSize da appBar

--- lib/app/theme/custom_theme/appbar_theme.dart
+++ lib/app/theme/custom_theme/appbar_theme.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kyw_management/app/routers/app_pages/app_pages_exports.dart';
 import 'package:kyw_management/utils/colors.dart';
 
 abstract class TAppbarTheme {
   static AppBarTheme light = AppBarTheme(
       elevation: 0,
+      titleTextStyle: GoogleFonts.roboto(fontSize: TConstants.fontSizeLg),
       backgroundColor: TColors.secondary,
       foregroundColor: TColors.base100,
       systemOverlayStyle: SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent));
