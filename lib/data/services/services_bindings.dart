1658d3084ba1bd4edaa0f239397cb1427e11673c
Kayky Barbosa <kaykyb629@gmail.com>
2024-05-26 19:00:28 -0300

    refactor: adicionando registro dos services

--- lib/data/services/services_bindings.dart
+++ lib/data/services/services_bindings.dart
@@ -1,13 +1,23 @@
 import 'package:get/get.dart';
 import 'package:kyw_management/app/controllers/app_controller.dart';
 import 'package:kyw_management/data/repositories/auth/auth_repository.dart';
+import 'package:kyw_management/data/repositories/message_repository.dart';
 import 'package:kyw_management/data/services/configure_login_service.dart';
+import 'package:kyw_management/data/services/message_service.dart';
 import 'package:kyw_management/data/services/refresh_token_service.dart';
+import 'package:kyw_management/data/services/web_socket_client.dart';
 
 class ServicesBindings implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<IAuthSettingsService>(() => AuthSettingsService(AppController.instance));
+    Get.put(WebSocketClient());
+    Get.put<IMessageService>(
+        MessageService(
+          messageService: IMessageRepository.instance,
+          websocket: WebSocketClient.instance,
+        ),
+        permanent: true);
     Get.lazyPut<IRefreshTokenService>(() => RefreshTokenService(authRepository: IAuthRepository.instance));
+    Get.lazyPut<IAuthSettingsService>(() => AuthSettingsService(AppController.instance));
   }
 }
