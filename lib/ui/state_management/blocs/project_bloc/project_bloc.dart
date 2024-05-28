60d9d6e315c22801ecb5e09a3a31300b3317a84c
Kayky Barbosa <kayky@abrem.net>
2024-05-28 14:14:25 -0300

    refactor: removendo inicialização da conexão websocket do projectBloc

--- lib/ui/state_management/blocs/project_bloc/project_bloc.dart
+++ lib/ui/state_management/blocs/project_bloc/project_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-import 'dart:convert';
 import 'dart:developer';
 
 import 'package:equatable/equatable.dart';
@@ -10,7 +8,6 @@ import 'package:kyw_management/data/repositories/project_repository.dart';
 import 'package:kyw_management/data/services/message_service.dart';
 import 'package:kyw_management/data/services/web_socket_client.dart';
 import 'package:kyw_management/domain/models/project_model.dart';
-import 'package:stomp_dart_client/stomp_dart_client.dart';
 
 part 'project_event.dart';
 part 'project_state.dart';
@@ -20,9 +17,11 @@ class ProjectBloc extends Bloc<ProjectEvent, ProjectState> {
     required IProjectRepository projectRepository,
     required WebSocketClient webSocket,
     required IMessageService messageService,
+    required AppController appController,
   })  : _repository = projectRepository,
         _ws = webSocket,
         _messageService = messageService,
+        _controller = appController,
         super(ProjectState()) {
     on<GetAllProjects>(_onGetAllProject);
     on<AddProject>(_onAddProject);
@@ -33,6 +32,7 @@ class ProjectBloc extends Bloc<ProjectEvent, ProjectState> {
   final IProjectRepository _repository;
   final WebSocketClient _ws;
   final IMessageService _messageService;
+  final AppController _controller;
 
   void _onGetAllProject(GetAllProjects event, Emitter<ProjectState> emit) async {
     if (state.allProjects.content.isNotEmpty) return;
@@ -68,64 +68,21 @@ class ProjectBloc extends Bloc<ProjectEvent, ProjectState> {
     );
   }
 
-  void _cwonnectWebSocket(ConnectWebSocket event, Emitter<ProjectState> emit) {
-    _ws.connect(
-      'ws://10.0.0.100:8080/connect',
-      {
-        // 'Authorization': 'Bearer ....',
-      },
-    );
+  void _connectWebSocket(ConnectWebSocket event, Emitter<ProjectState> emit) {
+    final projects = state.allProjects.content;
+    if (projects.isNotEmpty) {
+      for (var project in projects) {
+        log('Subscribe in project: ${project.id}', name: 'WEBSOCKET');
 
-    _messageService.subscribeToMessageUpdates((Map<String, dynamic> message) {
-      log('Received:  $message', name: 'WEBSOCKET');
-    });
+        _ws.subscribe(
+          '/project/${project.id}',
+          callback: (frame) => {log('Message received: ${frame.body}')},
+        );
+      }
+    }
   }
 
-  late StompClient stompClient;
-
-  void _connectWebSocket(ConnectWebSocket event, Emitter<ProjectState> emit) async {
-    final user = await AppController.instance.currentUser;
-
-    log('INICIANDO CONEXÃO...', name: 'WEBSOCKET');
-
-    stompClient = StompClient(
-      config: StompConfig(
-        url: 'ws://10.0.0.100:8080/connect',
-        onConnect: onConnect,
-        onWebSocketError: (dynamic error) => log(error.toString(), name: 'WEBSOCKET'),
-        // stompConnectHeaders: {'Authorization': 'Bearer ${user?.token}'},
-        // webSocketConnectHeaders: {'Authorization': 'Bearer ${user?.token}'},
-      ),
-    );
-
-    stompClient.activate();
-  }
-
-  void onConnect(StompFrame frame) async {
-    final user = await AppController.instance.currentUser;
-
-    log('CHAMANDO FUNÇÃO DEPOIS DE CONECTADO...', name: 'WEBSOCKET');
-
-    stompClient.subscribe(
-      destination: '/project/6a670038-1d35-44d4-bfc5-1be457a4f856',
-      callback: (frame) {
-        final result = json.decode(frame.body ?? '');
-        log('$result', name: 'WEBSOCKET');
-      },
-      headers: {'Authorization': 'Bearer ${user?.token}'},
-    );
-
-    Timer.periodic(const Duration(seconds: 5), (_) {
-      log('ENVIANDO MENSAGEM', name: 'WEBSOCKET');
-
-      stompClient.send(
-        destination: '/chat/project/6a670038-1d35-44d4-bfc5-1be457a4f856',
-        body: json.encode({
-          'sender': '${user?.id}',
-          'content': 'mensagem',
-        }),
-        headers: {'Authorization': 'Bearer ${user?.token}'},
-      );
-    });
+  void _sendMessage() {
+    _ws.send('/');
   }
 }
