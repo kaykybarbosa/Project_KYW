ca868a48ca356dca14f8467fad3e7fbfeccc965d
Kayky Barbosa <kayky@abrem.net>
2024-05-28 14:16:53 -0300

    refactor: alterando implementação do webSockertClient para o stompClient

--- lib/data/services/web_socket_client.dart
+++ lib/data/services/web_socket_client.dart
@@ -1,15 +1,13 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:developer';
 
 import 'package:get/get.dart';
-import 'package:web_socket_channel/io.dart';
+import 'package:stomp_dart_client/stomp_dart_client.dart';
 
 class WebSocketClient {
   static WebSocketClient get instance => Get.find();
 
-  IOWebSocketChannel? channel;
-
+  StompClient? stompClient;
   late StreamController<Map<String, dynamic>> messageController;
 
   WebSocketClient() {
@@ -20,54 +18,68 @@ class WebSocketClient {
     messageController = StreamController<Map<String, dynamic>>.broadcast();
   }
 
-  void connect(
-    String url,
-    Map<String, String> headers,
-  ) {
-    if (channel != null && channel!.closeCode == null) {
+  void connect(String url, {Map<String, String>? headers, Function(StompFrame)? onConnect}) {
+    if (stompClient != null && stompClient!.connected) {
       log('Already connected', name: 'WEBSOCKET');
       return;
     }
 
-    log('Connecting to the server...', name: 'WEBSOCKET');
-    channel = IOWebSocketChannel.connect(url, headers: headers);
+    log('Connecting to the server in URL: $url', name: 'WEBSOCKET');
 
-    channel!.stream.listen(
-      (event) {
-        Map<String, dynamic> message = jsonDecode(event);
+    stompClient = StompClient(
+      config: StompConfig(
+        url: '$url/connect',
+        onConnect: onConnect ?? (_) => log('Connect to the server in URL: $url/connect', name: 'WEBSOCKET'),
+        onWebSocketError: (dynamic error) => log(error.toString(), name: 'WEBSOCKET'),
+        // stompConnectHeaders: {'Authorization': 'Bearer ${user?.token}'},
+        // webSocketConnectHeaders: {'Authorization': 'Bearer ${user?.token}'},
+      ),
+    );
 
-        if (message['event'] == 'message.created') {
-          messageController.add(message['data']);
-        }
-      },
-      onDone: () {
-        log('Connection closed', name: 'WEBSOCKET');
-      },
-      onError: (error) {
-        log('Error: $error', name: 'WEBSOCKET');
-      },
+    stompClient!.activate();
+  }
+
+  void send(String destination, {String? body, String? token}) {
+    if (!_filter()) return;
+
+    stompClient!.send(
+      destination: destination,
+      body: body,
+      headers: {'Authorization': 'Bearer $token'},
     );
   }
 
-  void send(String data) {
-    if (channel == null || channel!.closeCode != null) {
-      log('Not connected', name: 'WEBSOCKET');
-      return;
-    }
+  void subscribe(
+    String destination, {
+    String? token,
+    required void Function(StompFrame) callback,
+  }) {
+    if (!_filter()) return;
 
-    channel!.sink.add(data);
+    stompClient!.subscribe(
+      destination: destination,
+      callback: callback,
+      headers: {'Authorization': 'Bearer $token'},
+    );
   }
 
   Stream<Map<String, dynamic>> messageUpdates() => messageController.stream;
 
   void disconnect() {
-    if (channel == null || channel!.closeCode != null) {
-      log('Not connected', name: 'WEBSOCKET');
-      return;
-    }
-    channel!.sink.close();
+    if (!_filter()) return;
+
+    stompClient!.deactivate();
 
     messageController.close();
     _initializeControllers();
+  }
+
+  bool _filter() {
+    if (stompClient == null || !stompClient!.connected) {
+      log('Not connected', name: 'WEBSOCKET');
+      return false;
+    }
+
+    return true;
   }
 }
