bccc020e3fc1161567b63b778e8a4511a14de99c
Kayky Barbosa <kayky@abrem.net>
2024-05-29 13:57:18 -0300

    refactor: refatorando service de mensagens

--- lib/data/services/message_service.dart
+++ lib/data/services/message_service.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:kyw_management/data/dtos/request/message_request.dart';
@@ -12,9 +11,15 @@ import 'package:result_dart/result_dart.dart';
 abstract class IMessageService {
   static IMessageService get instance => Get.find<IMessageService>();
 
-  AsyncResult<Unit, ApiException> createMessage(MessageRequest message);
+  AsyncResult<Unit, ApiException> sendMessage({
+    required String projectId,
+    required MessageRequest message,
+  });
 
-  void subscribeToMessageUpdates(Function(Map<String, dynamic>) onMessageReceived);
+  void subscribeToMessageUpdates({
+    required String projectId,
+    required Function(Map<String, dynamic>) onMessageReceived,
+  });
 
   void unsubscribeFromMessageUpdates();
 }
@@ -31,11 +36,15 @@ class MessageService implements IMessageService {
   StreamSubscription? _messageSubscription;
 
   @override
-  AsyncResult<Unit, ApiException> createMessage(MessageRequest message) async {
+  AsyncResult<Unit, ApiException> sendMessage({
+    required String projectId,
+    required MessageRequest message,
+  }) async {
     try {
-      var payload = {'event': 'message.create', 'data': message.toJson()};
-
-      _ws.send(jsonEncode(payload));
+      _ws.send(
+        '/chat/project/$projectId',
+        body: message.toJson(),
+      );
 
       return unit.toSuccess();
     } catch (e) {
@@ -43,20 +52,22 @@ class MessageService implements IMessageService {
     }
   }
 
-  Future<List<Message>> fetchMessages(String chatRoomId) async {
+  Future<List<Message>> getMessages(String projectId) async {
     final result = await _message.getMessages();
 
     return result.getOrNull() ?? [];
   }
 
   @override
-  void subscribeToMessageUpdates(
-    void Function(Map<String, dynamic>) onMessageReceived,
-  ) {
-    _messageSubscription = _ws.messageUpdates().listen(
-      (message) {
-        onMessageReceived(message);
-      },
+  void subscribeToMessageUpdates({
+    required String projectId,
+    required Function(Map<String, dynamic>) onMessageReceived,
+  }) {
+    _messageSubscription = _ws.messageUpdates().listen((message) => onMessageReceived(message));
+
+    _ws.subscribe(
+      '/project/$projectId',
+      callback: (frame) => onMessageReceived({'body': frame.body}),
     );
   }
 
