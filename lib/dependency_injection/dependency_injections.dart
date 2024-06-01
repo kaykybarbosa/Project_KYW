1988997aaaaf214d60d91c5cc50a38adcaf413bc
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-01 19:44:32 -0300

    feat: registrando instância do store para o db

--- lib/dependency_injection/dependency_injections.dart
+++ lib/dependency_injection/dependency_injections.dart
@@ -1,17 +1,21 @@
 import 'package:get/get.dart';
 import 'package:kyw_management/app/controllers/app_controller.dart';
 import 'package:kyw_management/app/routers/app_pages/app_pages_exports.dart';
+import 'package:kyw_management/data/db/database_bindings.dart';
 import 'package:kyw_management/data/repositories/repositories_bindings.dart';
 import 'package:kyw_management/data/services/storage_service/storages_service_bindings.dart';
 import 'package:kyw_management/data/services/web_socket_client.dart';
 import 'package:kyw_management/data/storages/storages_bindings.dart';
 import 'package:kyw_management/env/env.dart';
+import 'package:kyw_management/objectbox.g.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> dependencyInjections() async {
   await _initSharedPrefs();
+  await _initStoreToDB();
 
+  DatabaseBindings().dependencies();
   StoragesServiceBindings().dependencies();
   StoragesBindings().dependencies();
 
@@ -38,4 +42,10 @@ Future<void> _configuration() async {
 
 void _initWebSocket() {
   WebSocketClient.instance.connect(Env.WEBSOCKET);
+}
+
+Future<void> _initStoreToDB() async {
+  final store = await openStore();
+
+  Get.put<Store>(store);
 }
