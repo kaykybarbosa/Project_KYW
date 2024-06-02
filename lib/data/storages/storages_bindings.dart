c7f8e84854d33e98e484317fd04e97430b57112d
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-02 18:38:51 -0300

    refactor: removendo registro do messageStorage

--- lib/data/storages/storages_bindings.dart
+++ lib/data/storages/storages_bindings.dart
@@ -1,17 +1,13 @@
 import 'package:get/get.dart';
-import 'package:kyw_management/app/controllers/app_controller.dart';
 import 'package:kyw_management/data/services/storage_service/security_storage_service.dart';
 import 'package:kyw_management/data/services/storage_service/shared_prefs_service.dart';
 import 'package:kyw_management/data/storages/configure_app_storage.dart';
 import 'package:kyw_management/data/storages/current_user_storage.dart';
-import 'package:kyw_management/data/storages/db/database.dart';
-import 'package:kyw_management/data/storages/message_store.dart';
 
 class StoragesBindings implements Bindings {
   @override
   void dependencies() {
     Get.put<ICurrentUserStorage>(CurrentUserStorage(storageService: ISecurityStorageService.instance));
     Get.put<IConfigureAppStorage>(ConfigureAppStorage(sharedPrefsService: ISharedPrefsService.instance));
-    Get.put<IMessageStore>(MessageStore(IDatabase.instance, AppController.instance));
   }
 }
