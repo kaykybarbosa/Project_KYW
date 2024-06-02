3cc1a2a65ccffc6f89be10b2920d1e3a0d651890
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-02 18:38:19 -0300

    feat: adicionando função para formatar horário

--- lib/utils/formaters.dart
+++ lib/utils/formaters.dart
@@ -3,5 +3,8 @@ import 'package:intl/intl.dart';
 abstract class Formaters {
   static final DateFormat _formatter = DateFormat("dd/MM/yyyy");
 
+  /// Retorna um dataTime no formato: dd/mm/yyyy
   static String formatDate(DateTime date) => _formatter.format(date);
+
+  static String formatDateHours(DateTime date) => DateFormat("HH:mm").format(date);
 }
