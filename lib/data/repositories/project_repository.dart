9969d24bf1cf1dd9589fb68b656614bba5100113
Kayky Barbosa <kayky@abrem.net>
2024-06-07 20:16:09 -0300

    feat: implementando método de criar task

--- lib/data/repositories/project_repository.dart
+++ lib/data/repositories/project_repository.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
-import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:kyw_management/data/dtos/request/create_project_request.dart';
 import 'package:kyw_management/data/dtos/response/all_projects_response.dart';
 import 'package:kyw_management/data/dtos/response/detail_project_response.dart';
 import 'package:kyw_management/data/services/http_service/http_service.dart';
@@ -8,6 +10,8 @@ import 'package:result_dart/result_dart.dart';
 
 abstract class IProjectRepository {
   static IProjectRepository get instance => Get.find<IProjectRepository>();
+
+  AsyncResult<Unit, ApiException> createProject(CreateProjectRequest request);
 
   AsyncResult<AllProjectsResponse, ApiException> getAllProjects(String userId);
 
@@ -22,6 +26,28 @@ class ProjectRepository implements IProjectRepository {
   ProjectRepository(IHttpService httpService) : _http = httpService;
 
   final IHttpService _http;
+
+  @override
+  AsyncResult<Unit, ApiException> createProject(CreateProjectRequest request) async {
+    try {
+      if (request.image != null) {
+        final image = await MultipartFile.fromFile(request.image!.path);
+      }
+
+      final formData = FormData.fromMap(request.toMap(''));
+
+      await _http.post(
+        '${_http.baseUrl}/projects',
+        data: formData,
+      );
+
+      return const Success(unit);
+    } on DioException catch (e) {
+      return ApiException(message: e.message).toFailure();
+    } catch (e) {
+      return ApiException(message: e.toString()).toFailure();
+    }
+  }
 
   @override
   AsyncResult<AllProjectsResponse, ApiException> getAllProjects(String userId) async {
