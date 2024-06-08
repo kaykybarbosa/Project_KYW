c8a806317415625789b1e09acc9b2fed338e96b6
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-08 09:00:21 -0300

    refactor: criando toMapImage para UserRegisterRequest

--- lib/data/requests_models/user_register_request.dart
+++ lib/data/requests_models/user_register_request.dart
@@ -1,20 +1,34 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+
 class UserRegisterRequest {
   UserRegisterRequest({
     required this.nickname,
     required this.email,
     required this.password,
     this.phone,
+    this.image,
   });
 
   final String nickname;
   final String email;
   final String password;
   final String? phone;
+  final File? image;
 
   Map<String, dynamic> toMap() => {
         'nickname': nickname,
         'email': email,
         'password': password,
         'phone': phone,
+      };
+
+  Map<String, dynamic> toMapImage(MultipartFile image) => {
+        'nickname': nickname,
+        'email': email,
+        'password': password,
+        'phone': phone,
+        'image': image,
       };
 }
