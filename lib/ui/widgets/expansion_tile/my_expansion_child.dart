0d2e51e2242918eeb0e959f00625f6d444b82927
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-08 20:44:23 -0300

    feat: extraindo widgets em componentes

---
+++ lib/ui/widgets/expansion_tile/my_expansion_child.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:kyw_management/utils/colors.dart';
+import 'package:kyw_management/utils/constants.dart';
+
+class MyExpansionChild extends StatelessWidget {
+  const MyExpansionChild({super.key, required this.child, this.isFirst = true, this.isLast = true});
+
+  final bool isFirst;
+  final bool isLast;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          color: TColors.base100,
+          borderRadius: BorderRadius.vertical(
+            bottom: isLast ? const Radius.circular(TConstants.cardRadiusXs) : Radius.zero,
+          ),
+          border: Border(
+            top: isFirst ? const BorderSide(color: TColors.base200) : BorderSide.none,
+            bottom: isLast ? BorderSide.none : const BorderSide(color: TColors.base200),
+          ),
+        ),
+        child: child,
+      );
+}
---
+++ lib/ui/widgets/text_field/title_text_field.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:kyw_management/utils/constants.dart';
+
+class TitleTextField extends StatelessWidget {
+  const TitleTextField(this.text, {super.key, this.padding});
+
+  final String? text;
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: padding ?? const EdgeInsets.only(left: 2, bottom: 4),
+        child: Text(
+          text!,
+          style: TextStyle(
+            fontSize: TConstants.fontSizeLg - 1,
+            fontWeight: FontWeight.w500,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      );
+}
