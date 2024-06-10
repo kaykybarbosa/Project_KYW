bc46eb15fddd06fb7b1120092894df6dee2789c3
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-09 21:14:38 -0300

    feat adicionando método de transformar type string em dateTime

--- lib/utils/formaters.dart
+++ lib/utils/formaters.dart
@@ -1,10 +1,13 @@
 import 'package:intl/intl.dart';
 
-abstract class Formaters {
+abstract class Formatters {
   static final DateFormat _formatter = DateFormat("dd/MM/yyyy");
 
   /// Retorna um dataTime no formato: dd/mm/yyyy
   static String formatDate(DateTime date) => _formatter.format(date);
+
+  static DateTime? tryParseDate(String date, {bool dateDefault = false}) =>
+      dateDefault ? DateTime.tryParse(date) : _formatter.tryParse(date);
 
   static String formatDateHours(DateTime date) => DateFormat("HH:mm").format(date);
 }
