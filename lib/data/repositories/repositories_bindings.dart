20a95047d02e9a4d95680c96b7a9c83ec60fe4c4
Kayky Barbosa <kayky@abrem.net>
2024-06-10 15:28:20 -0300

    feat: registrando instância do TaskRepository

--- lib/data/repositories/repositories_bindings.dart
+++ lib/data/repositories/repositories_bindings.dart
@@ -1,12 +1,23 @@
 import 'package:get/get.dart';
 import 'package:kyw_management/data/repositories/message_repository.dart';
 import 'package:kyw_management/data/repositories/project_repository.dart';
+import 'package:kyw_management/data/repositories/task_repository.dart';
 import 'package:kyw_management/data/services/http_service/http_service.dart';
 
 class RepositoriesBindings implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<IMessageRepository>(() => MessageRepository(IHttpService.instanceAuth));
-    Get.lazyPut<IProjectRepository>(() => ProjectRepository(IHttpService.instanceAuth));
+    Get.lazyPut<IMessageRepository>(
+      () => MessageRepository(IHttpService.instanceAuth),
+      fenix: true,
+    );
+    Get.lazyPut<IProjectRepository>(
+      () => ProjectRepository(IHttpService.instanceAuth),
+      fenix: true,
+    );
+    Get.lazyPut<ITaskRepository>(
+      () => TaskRepository(IHttpService.instanceAuth),
+      fenix: true,
+    );
   }
 }
