d17a8f363e16ae2133d8b96b24e7d8332ce06ae9
Kayky Barbosa <kayky@abrem.net>
2024-06-10 18:40:01 -0300

    feat: adicionando cubit para os gerenciamento das tasks

---
+++ lib/ui/state_management/cubits/task_cubit/task_state.dart
@@ -0,0 +1,10 @@
+part of 'task_cubit.dart';
+
+class TaskState extends Equatable {
+  const TaskState();
+
+  // final List<TaskResponse> tasksOficial;
+
+  @override
+  List<Object> get props => [];
+}
