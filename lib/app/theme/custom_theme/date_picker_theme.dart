71c147fffdc3666ba59273791a0df18fc3ab99e9
Kayky Barbosa <kayky@abrem.net>
2024-06-10 15:26:07 -0300

    style: melhorando tema do datePicker

--- lib/app/theme/custom_theme/date_picker_theme.dart
+++ lib/app/theme/custom_theme/date_picker_theme.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:kyw_management/utils/colors.dart';
 
 abstract class TDatePickerTheme {
-  static DatePickerThemeData light = const DatePickerThemeData(
+  static DatePickerThemeData light = DatePickerThemeData(
+    backgroundColor: TColors.base100,
     headerBackgroundColor: TColors.secondary,
-    headerForegroundColor: TColors.base200,
-    backgroundColor: TColors.base200,
+    headerForegroundColor: TColors.base100,
+    weekdayStyle: const TextStyle(color: TColors.base300),
+    dayForegroundColor: WidgetStateProperty.resolveWith<Color>(
+      (Set<WidgetState> states) {
+        final isSelected = states.contains(WidgetState.selected);
+
+        return isSelected ? TColors.base100 : TColors.secondary;
+      },
+    ),
   );
 }
