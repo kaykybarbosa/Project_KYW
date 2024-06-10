0e034b4e39a143d67729387eea5f2532ad8414df
Kayky Barbosa <kayky@abrem.net>
2024-06-10 18:37:42 -0300

    feat: implementando os método getter e update das task em TaskRepository

--- lib/data/repositories/task_repository.dart
+++ lib/data/repositories/task_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:kyw_management/data/dtos/request/create_task_request.dart';
+import 'package:kyw_management/data/dtos/response/all_tasks_response.dart';
 import 'package:kyw_management/data/services/http_service/http_service.dart';
 import 'package:kyw_management/domain/exception/api_exception.dart';
 import 'package:result_dart/result_dart.dart';
@@ -8,13 +9,44 @@ import 'package:result_dart/result_dart.dart';
 abstract class ITaskRepository {
   static ITaskRepository get instance => Get.find();
 
+  AsyncResult<AllTasksResponse, ApiException> getAllTasks();
+
+  AsyncResult<TaskResponse, ApiException> getTaskById(String taskId);
+
   AsyncResult<Unit, ApiException> createTask(CreateTaskRequest request);
+
+  AsyncResult<TaskResponse, ApiException> updateTask(TaskResponse task);
 }
 
 class TaskRepository implements ITaskRepository {
   TaskRepository(IHttpService httpService) : _http = httpService;
 
   final IHttpService _http;
+  @override
+  AsyncResult<AllTasksResponse, ApiException> getAllTasks() async {
+    try {
+      final result = await _http.get('${_http.baseUrl}/tasks');
+
+      return AllTasksResponse.fromMap(result.data).toSuccess();
+    } on DioException catch (e) {
+      return ApiException(message: e.message).toFailure();
+    } catch (e) {
+      return ApiException(message: e.toString()).toFailure();
+    }
+  }
+
+  @override
+  AsyncResult<TaskResponse, ApiException> getTaskById(String taskId) async {
+    try {
+      final result = await _http.get('${_http.baseUrl}/tasks/$taskId');
+
+      return TaskResponse.fromMap(result.data).toSuccess();
+    } on DioException catch (e) {
+      return ApiException(message: e.message).toFailure();
+    } catch (e) {
+      return ApiException(message: e.toString()).toFailure();
+    }
+  }
 
   @override
   AsyncResult<Unit, ApiException> createTask(CreateTaskRequest request) async {
@@ -25,6 +57,22 @@ class TaskRepository implements ITaskRepository {
       );
 
       return const Success(unit);
+    } on DioException catch (e) {
+      return ApiException(message: e.message).toFailure();
+    } catch (e) {
+      return ApiException(message: e.toString()).toFailure();
+    }
+  }
+
+  @override
+  AsyncResult<TaskResponse, ApiException> updateTask(TaskResponse task) async {
+    try {
+      final result = await _http.post(
+        '${_http.baseUrl}/tasks',
+        data: task.toJson(),
+      );
+
+      return Success(TaskResponse.fromJson(result));
     } on DioException catch (e) {
       return ApiException(message: e.message).toFailure();
     } catch (e) {
