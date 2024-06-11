729241591dab01e5ac1fd3109176379b956567a7
Kayky Barbosa <kayky@abrem.net>
2024-06-11 18:19:22 -0300

    refactor: corrigindo imagem de um projeto

--- lib/ui/widgets/card_project.dart
+++ lib/ui/widgets/card_project.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:kyw_management/app/controllers/app_controller.dart';
 import 'package:kyw_management/app/routers/app_pages/app_pages_exports.dart';
 import 'package:kyw_management/data/dtos/response/all_projects_response.dart';
+import 'package:kyw_management/env/env.dart';
 import 'package:kyw_management/utils/colors.dart';
 import 'package:kyw_management/utils/constants.dart';
 import 'package:kyw_management/utils/icons.dart';
@@ -20,90 +18,103 @@ class CardProject extends StatelessWidget {
   final Function()? onTap;
 
   Widget _getImage() {
-    return Image.asset(
-      'assets/group.png',
-      width: 50,
-      fit: BoxFit.cover,
-    );
-    if (project.imageUrl != null) {
-      final file = File(AppController.instance.directory.path);
-      file.writeAsString(project.imageUrl ?? '');
+    const width = 45.0;
+    const height = 45.0;
 
-      return CircleAvatar(
-        radius: 50,
-        backgroundColor: TColors.base300,
-        backgroundImage: FileImage(File("filePath")),
+    if (project.imageUrl != null) {
+      final image = project.imageUrl!.replaceAll(
+        RegExp(r'localhost'),
+        Env.LOCALHOST,
+      );
+
+      return Container(
+        color: TColors.base120,
+        width: width,
+        height: height,
+        child: Image.network(
+          image,
+          width: width,
+          height: height,
+          cacheWidth: 123,
+          cacheHeight: 123,
+          fit: BoxFit.cover,
+        ),
       );
     } else {
       return Image.asset(
         'assets/group.png',
-        width: 50,
+        width: width,
+        height: height,
         fit: BoxFit.cover,
       );
     }
   }
 
   @override
-  Widget build(BuildContext context) => InkWell(
-        onTap: onTap,
-        borderRadius: BorderRadius.circular(TConstants.cardRadiusXs),
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 2.5),
-          decoration: BoxDecoration(
-            border: const Border(
-              left: BorderSide(
-                color: TColors.secondary,
-                width: 6,
-              ),
+  Widget build(BuildContext context) {
+    debugInvertOversizedImages = true;
+
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(TConstants.cardRadiusXs),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 2.5),
+        decoration: BoxDecoration(
+          border: const Border(
+            left: BorderSide(
+              color: TColors.secondary,
+              width: 6,
             ),
-            borderRadius: BorderRadius.circular(TConstants.cardRadiusXs),
           ),
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: 8,
-            ),
-            decoration: BoxDecoration(
-                color: TColors.base100,
-                border: const Border(
-                  top: BorderSide(color: TColors.base200),
-                  right: BorderSide(color: TColors.base200),
-                  bottom: BorderSide(color: TColors.base200),
-                ),
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(TConstants.radiusLeftInput),
-                  bottomLeft: Radius.circular(TConstants.radiusLeftInput),
-                  topRight: Radius.circular(TConstants.cardRadiusXs),
-                  bottomRight: Radius.circular(TConstants.cardRadiusXs),
-                ),
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                    color: TColors.base900.withOpacity(.16),
-                    blurRadius: TConstants.blurRadius,
-                  )
-                ]),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                /// Imagem do grupo
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: _getImage(),
-                ),
+          borderRadius: BorderRadius.circular(TConstants.cardRadiusXs),
+        ),
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10,
+            vertical: 8,
+          ),
+          decoration: BoxDecoration(
+              color: TColors.base100,
+              border: const Border(
+                top: BorderSide(color: TColors.base200),
+                right: BorderSide(color: TColors.base200),
+                bottom: BorderSide(color: TColors.base200),
+              ),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(TConstants.radiusLeftInput),
+                bottomLeft: Radius.circular(TConstants.radiusLeftInput),
+                topRight: Radius.circular(TConstants.cardRadiusXs),
+                bottomRight: Radius.circular(TConstants.cardRadiusXs),
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: TColors.base900.withOpacity(.16),
+                  blurRadius: TConstants.blurRadius,
+                )
+              ]),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              /// Imagem do grupo
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: _getImage(),
+              ),
 
-                /// Nome do grupo
-                Expanded(
-                  flex: 5,
-                  child: _GroupName(project: project),
-                ),
+              /// Nome do grupo
+              Expanded(
+                flex: 5,
+                child: _GroupName(project: project),
+              ),
 
-                /// Está fixado
-                _MySufix(project: project)
-              ],
-            ),
+              /// Está fixado
+              _MySufix(project: project)
+            ],
           ),
         ),
-      );
+      ),
+    );
+  }
 }
 
 class _GroupName extends StatelessWidget {
@@ -121,10 +132,12 @@ class _GroupName extends StatelessWidget {
             Text(
               project.name,
               style: const TextStyle(
-                fontSize: TConstants.fontSizeLg,
+                fontSize: TConstants.fontSizeLg - 1,
                 fontWeight: FontWeight.bold,
                 color: TColors.secondary,
               ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
             ),
 
             // Última mensagem enviada
@@ -134,7 +147,7 @@ class _GroupName extends StatelessWidget {
                   child: Text(
                     'Você criou este projeto',
                     style: TextStyle(
-                      fontSize: TConstants.fontSizeMd,
+                      fontSize: TConstants.fontSizeMd - 1,
                       color: TColors.base200,
                     ),
                     overflow: TextOverflow.ellipsis,
