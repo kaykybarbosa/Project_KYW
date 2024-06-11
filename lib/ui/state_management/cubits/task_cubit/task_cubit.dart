2e058b11318ec96cf30d52f3cbc70457cd9ce752
Kayky Barbosa <kayky@abrem.net>
2024-06-11 18:22:31 -0300

    feat: implementação do gerenciamento de estado das tasks

--- lib/ui/state_management/cubits/task_cubit/task_cubit.dart
+++ lib/ui/state_management/cubits/task_cubit/task_cubit.dart
@@ -1,8 +1,48 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:kyw_management/data/dtos/response/all_tasks_response.dart';
+import 'package:kyw_management/data/repositories/task_repository.dart';
 
 part 'task_state.dart';
 
 class TaskCubit extends Cubit<TaskState> {
-  TaskCubit() : super(const TaskState());
+  TaskCubit(ITaskRepository taskRepository)
+      : _repository = taskRepository,
+        super(const TaskState());
+
+  final ITaskRepository _repository;
+
+  void getAllTasks() async {
+    emit(state.copyWith(status: TaskCubitStatus.inProgress));
+
+    final result = await _repository.getAllTasks();
+
+    result.fold(
+      (success) => emit(state.copyWith(
+        tasksOficial: success,
+        status: TaskCubitStatus.success,
+      )),
+      (failure) => emit(state.copyWith(
+        status: TaskCubitStatus.failure,
+        errorMessage: failure.message,
+      )),
+    );
+  }
+
+  void getTaskById(String taskId) async {
+    emit(state.copyWith(status: TaskCubitStatus.inProgress));
+
+    final result = await _repository.getTaskById(taskId);
+
+    result.fold(
+      (success) => emit(state.copyWith(
+        taskDetails: success,
+        status: TaskCubitStatus.success,
+      )),
+      (failure) => emit(state.copyWith(
+        status: TaskCubitStatus.failure,
+        errorMessage: failure.message,
+      )),
+    );
+  }
 }
--- lib/ui/state_management/cubits/task_cubit/task_state.dart
+++ lib/ui/state_management/cubits/task_cubit/task_state.dart
@@ -1,10 +1,56 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'task_cubit.dart';
 
 class TaskState extends Equatable {
-  const TaskState();
+  const TaskState({
+    this.tasksOficial = const AllTasksResponse(),
+    this.tasks = const AllTasksResponse(),
+    this.taskDetails = const TaskResponse(),
+    this.status = TaskCubitStatus.initial,
+    this.errorMessage,
+  });
 
-  // final List<TaskResponse> tasksOficial;
+  final AllTasksResponse tasksOficial;
+  final AllTasksResponse tasks;
+  final TaskResponse taskDetails;
+  final TaskCubitStatus status;
+  final String? errorMessage;
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [
+        tasksOficial,
+        tasks,
+        status,
+        taskDetails,
+        errorMessage,
+      ];
+
+  TaskState copyWith({
+    AllTasksResponse? tasksOficial,
+    AllTasksResponse? tasks,
+    TaskResponse? taskDetails,
+    TaskCubitStatus? status,
+    String? errorMessage,
+  }) {
+    return TaskState(
+      tasksOficial: tasksOficial ?? this.tasksOficial,
+      tasks: tasks ?? this.tasksOficial,
+      taskDetails: taskDetails ?? this.taskDetails,
+      status: status ?? TaskCubitStatus.initial,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+}
+
+enum TaskCubitStatus {
+  initial,
+  success,
+  failure,
+  inProgress,
+}
+
+extension TaskStatusX on TaskCubitStatus {
+  bool get isSuccess => this == TaskCubitStatus.success;
+  bool get isFailure => this == TaskCubitStatus.failure;
+  bool get isInProgress => this == TaskCubitStatus.inProgress;
 }
