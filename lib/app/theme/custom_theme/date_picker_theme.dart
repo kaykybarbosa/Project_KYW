d10f666c6ca59dd54f25460bbe0a1891a6a1494c
Kayky Barbosa <kayky@abrem.net>
2024-06-11 10:41:53 -0300

    refactor: adicionando cor para os dias desabilitados

--- lib/app/theme/custom_theme/date_picker_theme.dart
+++ lib/app/theme/custom_theme/date_picker_theme.dart
@@ -3,15 +3,22 @@ import 'package:kyw_management/utils/colors.dart';
 
 abstract class TDatePickerTheme {
   static DatePickerThemeData light = DatePickerThemeData(
-    backgroundColor: TColors.base100,
+    backgroundColor: TColors.base120,
     headerBackgroundColor: TColors.secondary,
     headerForegroundColor: TColors.base100,
     weekdayStyle: const TextStyle(color: TColors.base300),
     dayForegroundColor: WidgetStateProperty.resolveWith<Color>(
       (Set<WidgetState> states) {
-        final isSelected = states.contains(WidgetState.selected);
+        Color color = TColors.secondary;
 
-        return isSelected ? TColors.base100 : TColors.secondary;
+        if (states.contains(WidgetState.disabled)) {
+          color = TColors.base150;
+        }
+        if (states.contains(WidgetState.selected)) {
+          color = TColors.base150;
+        }
+
+        return color;
       },
     ),
   );
