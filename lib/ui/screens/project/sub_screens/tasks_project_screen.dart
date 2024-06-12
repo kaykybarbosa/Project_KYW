b66d9e8d75f9857a84b77e5585c3cdb533616d2c
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-11 21:58:57 -0300

    feat: adicionando tela de load das task em tasksProjectScreen

--- lib/ui/screens/project/sub_screens/tasks_project_screen.dart
+++ lib/ui/screens/project/sub_screens/tasks_project_screen.dart
@@ -6,6 +6,7 @@ import 'package:kyw_management/ui/screens/tasks/widgets/filters_bar_tasks.dart';
 import 'package:kyw_management/ui/state_management/cubits/task_cubit/task_cubit.dart';
 import 'package:kyw_management/ui/widgets/card_task.dart';
 import 'package:kyw_management/ui/widgets/my_search_bar.dart';
+import 'package:kyw_management/ui/widgets/skelton_indicator.dart';
 import 'package:kyw_management/utils/colors.dart';
 import 'package:kyw_management/utils/icons.dart';
 
@@ -22,11 +23,11 @@ class TasksProjectScreen extends StatelessWidget {
           floatingActionButton: FloatingActionButton(
             tooltip: 'Criar task',
             backgroundColor: TColors.primary,
+            child: const Icon(TIcons.add),
             onPressed: () => Get.toNamed(
               AppRoutes.createTask,
               parameters: {'id': projectId},
             ),
-            child: const Icon(TIcons.add),
           ),
         ),
       );
@@ -37,75 +38,89 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => BlocBuilder<TaskCubit, TaskState>(
-        builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-                // horizontal: 15,
-                // vertical: 10
-                ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                state.tasksOficial.tasks.isNotEmpty
-                    ? const Expanded(
-                        child: CustomScrollView(
-                          slivers: <Widget>[
-                            /// Header
-                            _HeaderTasks(),
+        builder: (context, state) => Padding(
+          padding: const EdgeInsets.symmetric(
+              // horizontal: 15,
+              // vertical: 10
+              ),
+          child: switch (state.status) {
+            TaskCubitStatus.inProgress => const Text('data'),
+            _ => const _TasksInProgress(),
+          },
+        ),
+        // child:   Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     // _ => state.tasksOficial.tasks.isNotEmpty
+        //     //     ? const Expanded(
+        //     //         child: CustomScrollView(
+        //     //           slivers: <Widget>[
+        //     //             /// Header
+        //     //             _HeaderTasks(),
 
-                            /// Tarefas
-                            _Tasks()
-                          ],
-                        ),
-                      )
-                    : const _TasksEmpty(),
-              ],
-            ),
-          );
-        },
+        //     //             /// Tarefas
+        //     //             _Tasks()
+        //     //           ],
+        //     //         ),
+        //     //       )
+        //     //     : const _TasksEmpty(),
+        //   ],
+        // ),
       );
 }
 
-class _Tasks extends StatelessWidget {
-  const _Tasks();
+class _TasksInProgress extends StatelessWidget {
+  const _TasksInProgress();
 
   @override
-  Widget build(BuildContext context) => BlocBuilder<TaskCubit, TaskState>(
-        builder: (context, state) {
-          return SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return CardTask(task: state.tasks.tasks[index]);
-              },
-              childCount: state.tasks.tasks.length,
-            ),
-          );
-        },
-      );
-}
-
-class _TasksEmpty extends StatelessWidget {
-  const _TasksEmpty();
-
-  @override
-  Widget build(BuildContext context) => Center(
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: TConstants.defaultMargin),
         child: Column(
           children: <Widget>[
-            /// Título
-            const Text(
-              'Nenhuma task encontrada',
-              style: TextStyle(
-                fontSize: TConstants.fontSizeLg,
-                color: TColors.base300,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SkeltonIndicator(
+                  width: Get.width * .8,
+                  height: 40,
+                ),
+                const SkeltonIndicator(
+                  width: 40,
+                  height: 40,
+                ),
+              ],
             ),
-
-            const Gap(15),
-
-            /// Imagem
-            Image.asset(
-              'assets/tasks_empty.png',
-              fit: BoxFit.cover,
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SkeltonIndicator(
+                  width: 100,
+                  height: 40,
+                ),
+                SkeltonIndicator(
+                  width: 100,
+                  height: 40,
+                ),
+                SkeltonIndicator(
+                  width: 100,
+                  height: 40,
+                ),
+              ],
+            ),
+            const Gap(10),
+            Column(
+              children: List.generate(
+                3,
+                (index) => const Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5,
+                  ),
+                  child: SkeltonIndicator(
+                    width: double.infinity,
+                    height: 100,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
@@ -139,6 +154,56 @@ class _HeaderTasks extends StatelessWidget {
 
             /// Filtros
             const FiltersBarTasks(),
+          ],
+        ),
+      );
+}
+
+class _Tasks extends StatelessWidget {
+  const _Tasks();
+
+  @override
+  Widget build(BuildContext context) => BlocBuilder<TaskCubit, TaskState>(
+        builder: (context, state) => SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: TConstants.defaultMargin,
+                  vertical: 10,
+                ),
+                child: CardTask(task: state.tasks.tasks[index]),
+              );
+            },
+            childCount: state.tasks.tasks.length,
+          ),
+        ),
+      );
+}
+
+class _TasksEmpty extends StatelessWidget {
+  const _TasksEmpty();
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Column(
+          children: <Widget>[
+            /// Título
+            const Text(
+              'Nenhuma task encontrada',
+              style: TextStyle(
+                fontSize: TConstants.fontSizeLg,
+                color: TColors.base300,
+              ),
+            ),
+
+            const Gap(15),
+
+            /// Imagem
+            Image.asset(
+              'assets/tasks_empty.png',
+              fit: BoxFit.cover,
+            ),
           ],
         ),
       );
