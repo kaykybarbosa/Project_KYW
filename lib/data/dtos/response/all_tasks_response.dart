426950c8fd7980264de7da4bd346b81bee819d06
Kayky Barbosa <kayky@abrem.net>
2024-06-12 18:44:55 -0300

    refactor: atualização da api

--- lib/data/dtos/response/all_tasks_response.dart
+++ lib/data/dtos/response/all_tasks_response.dart
@@ -6,6 +6,7 @@ import 'package:kyw_management/data/dtos/project_include_reponse.dart';
 import 'package:kyw_management/data/dtos/user_include_response.dart';
 import 'package:kyw_management/domain/enums/criticality_enum.dart';
 import 'package:kyw_management/domain/enums/status.dart';
+import 'package:kyw_management/utils/formaters.dart';
 
 class AllTasksResponse {
   const AllTasksResponse({
@@ -41,9 +42,10 @@ class TaskResponse extends Equatable {
     this.id = '',
     this.status = TaskStatus.incomplete,
     this.criticality,
+    this.title = '',
     this.description = '',
     this.attachments,
-    this.attributedTo,
+    this.attributedTo = const [],
     this.pin = false,
     this.project,
     this.createAt,
@@ -53,19 +55,24 @@ class TaskResponse extends Equatable {
   final String id;
   final TaskStatus status;
   final CriticalityEnum? criticality;
-  final String description;
+  final String title;
+  final String? description;
   final String? attachments;
-  final UserIncludeResponse? attributedTo;
+  final List<UserIncludeResponse> attributedTo;
   final ProjectIncludeResponse? project;
   final bool pin;
   final DateTime? createAt;
   final DateTime? completedAt;
+
+  // GETTERS
+  String get createAtWithOptions => Formatters.formatDateWithOptions(createAt ?? DateTime.now());
 
   @override
   List<Object?> get props => [
         id,
         status,
         criticality,
+        title,
         description,
         attributedTo,
         attachments,
@@ -78,12 +85,17 @@ class TaskResponse extends Equatable {
         id: map['id'],
         status: TaskStatusM.fromMap(map['status']),
         criticality: map['criticality'] != null ? CriticalityEnumM.fromMap(map['criticality']) : null,
+        title: map['title'],
         description: map['description'],
         attachments: map['attachments'],
         createAt: map['createAt'] != null ? DateTime.parse(map['createAt']) : null,
         completedAt: map['completedAt'] != null ? DateTime.parse(map['completedAt']) : null,
         project: map['project'] != null ? ProjectIncludeResponse.fromMap(map['project']) : null,
-        attributedTo: map['attributedTo'] != null ? UserIncludeResponse.fromMap(map['attributedTo']) : null,
+        attributedTo: map['attributedTo'] != null
+            ? map['attributedTo']
+                .map<UserIncludeResponse>((member) => UserIncludeResponse.fromMap(map['attributedTo']))
+                .toList()
+            : [],
         pin: map['pin'],
       );
 
@@ -92,7 +104,7 @@ class TaskResponse extends Equatable {
         status: TaskStatus.incomplete,
         criticality: CriticalityEnum.high,
         description: 'description',
-        attributedTo: UserIncludeResponse(),
+        attributedTo: const [],
         project: ProjectIncludeResponse(),
         pin: false,
         createAt: DateTime(0000),
@@ -102,9 +114,10 @@ class TaskResponse extends Equatable {
         'id': id,
         'status': '',
         'criticality': CriticalityEnumM.toMap(criticality),
+        'title': title,
         'description': description,
         'attachments': attachments,
-        'attributedTo': attributedTo?.toMap(),
+        'attributedTo': attributedTo.map((member) => member.toMap()),
         'project': project?.toMap(),
         'pin': pin,
         'createAt': createAt?.millisecondsSinceEpoch,
