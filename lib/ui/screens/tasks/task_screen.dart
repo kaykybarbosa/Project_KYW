af5c9871dbe382ac2f253334f887ee0ac46720fa
Kayky Barbosa <kayky@abrem.net>
2024-06-13 21:04:05 -0300

    refactor: adicionando apenas as tasks atribuídas ao usuário atual

--- lib/ui/screens/tasks/task_screen.dart
+++ lib/ui/screens/tasks/task_screen.dart
@@ -1,68 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:kyw_management/app/routers/app_pages/app_pages_exports.dart';
+import 'package:kyw_management/data/dtos/response/all_tasks_response.dart';
 import 'package:kyw_management/domain/enums/screens.dart';
 import 'package:kyw_management/ui/screens/home/widgets/my_sliver_list.dart';
 import 'package:kyw_management/ui/screens/tasks/widgets/my_modal_filter_task.dart';
+import 'package:kyw_management/ui/state_management/cubits/task_cubit/task_cubit.dart';
 import 'package:kyw_management/ui/widgets/card_task.dart';
 import 'package:kyw_management/ui/widgets/my_order.dart';
 import 'package:kyw_management/ui/widgets/my_search_bar.dart';
 import 'package:kyw_management/ui/widgets/my_two_filters.dart';
+import 'package:kyw_management/ui/widgets/tasks_in_progress.dart';
 import 'package:kyw_management/utils/colors.dart';
 import 'package:kyw_management/utils/texts.dart';
 
-class TaskScreen extends StatelessWidget {
+class TaskScreen extends StatefulWidget {
   const TaskScreen({super.key});
 
-  Future<dynamic> myModalBottom(BuildContext context, Widget child) async {
-    await showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      backgroundColor: Colors.transparent,
-      builder: (BuildContext context) => child,
-    );
+  @override
+  State<TaskScreen> createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  void _getAllTasks() => context.read<TaskCubit>().getAllTasks();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getAllTasks();
   }
 
   @override
   Widget build(BuildContext context) {
-    final tasks = [];
-
-    return Scaffold(
-      body: tasks.isNotEmpty ? _Body() : const _TasksEmpty(),
+    return BlocBuilder<TaskCubit, TaskState>(
+      builder: (context, state) {
+        return Scaffold(
+          body: state.status.isInProgress
+              ? const TasksInProgress()
+              : state.tasks.isNotEmpty
+                  ? _Body(state.tasks)
+                  : const _TasksEmpty(),
+        );
+      },
     );
   }
 }
 
-class _TasksEmpty extends StatelessWidget {
-  const _TasksEmpty();
-
-  @override
-  Widget build(BuildContext context) => Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Nenhuma task encontrada',
-              style: TextStyle(
-                fontSize: TConstants.fontSizeLg,
-                color: TColors.base300,
-              ),
-            ),
-            const Gap(15),
-            Image.asset(
-              'assets/tasks_empty_2.png',
-              fit: BoxFit.cover,
-              width: Get.width * .7,
-              height: Get.width * .7,
-            ),
-          ],
-        ),
-      );
-}
-
 class _Body extends StatelessWidget {
-  _Body();
+  const _Body(this.tasks);
+
+  final List<TaskResponse> tasks;
 
   Future<dynamic> myModalBottom(
     BuildContext context,
@@ -75,8 +65,6 @@ class _Body extends StatelessWidget {
       builder: (BuildContext context) => child,
     );
   }
-
-  final tasks = [];
 
   @override
   Widget build(BuildContext context) => Column(
@@ -112,10 +100,43 @@ class _Body extends StatelessWidget {
               childCount: tasks.length,
               builder: (_, index) => Padding(
                 padding: EdgeInsets.only(bottom: index != tasks.length - 1 ? 10 : 0),
-                child: CardTask(task: tasks[index]),
+                child: CardTask(
+                  task: tasks[index],
+                  onTap: () => Get.toNamed(
+                    AppRoutes.taskDetails,
+                    parameters: {'id': tasks[index].id},
+                  ),
+                ),
               ),
             ),
           ),
         ],
+      );
+}
+
+class _TasksEmpty extends StatelessWidget {
+  const _TasksEmpty();
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Nenhuma task encontrada',
+              style: TextStyle(
+                fontSize: TConstants.fontSizeLg,
+                color: TColors.base300,
+              ),
+            ),
+            const Gap(15),
+            Image.asset(
+              'assets/tasks_empty_2.png',
+              fit: BoxFit.cover,
+              width: Get.width * .7,
+              height: Get.width * .7,
+            ),
+          ],
+        ),
       );
 }
